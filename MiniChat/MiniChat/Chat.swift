0a81a56acee926f9f0910ae4e9b2f547e3564e74
Akkshay Khoslaa <akhoslaa@berkeley.edu>
2016-10-18 22:25:39 -0700

    initial commit

---
+++ MiniChat/MiniChat/Chat.swift
@@ -0,0 +1,13 @@
+//
+//  Chat.swift
+//  MiniChat
+//
+//  Created by Akkshay Khoslaa on 10/18/16.
+//  Copyright © 2016 Akkshay Khoslaa. All rights reserved.
+//
+
+import Foundation
+
+class Chat {
+    
+}
